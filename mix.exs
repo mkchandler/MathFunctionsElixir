3cf098500b00e0a674c7bbb26fde862b402e46f0
Matt Chandler <mkchandler@gmail.com>
2017-03-20 21:52:33 -0500

    Added credo for static analysis

--- mix.exs
+++ mix.exs
@@ -11,24 +11,13 @@ defmodule MathFunctions.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:coverex, "~> 1.4.10", only: :test}]
+    [{:coverex, "~> 1.4.10", only: :test},
+     {:credo, "~> 0.7", only: [:dev, :test]}]
   end
 end
+
