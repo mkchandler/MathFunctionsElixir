c7c67c7a55df0bbc941182f79bbb2ee4f1a5a3e2
Matt Chandler <mkchandler@gmail.com>
2017-03-20 22:13:52 -0500

    use consistent conditional ordering like a sane person

--- test/math_functions_test.exs
+++ test/math_functions_test.exs
@@ -4,7 +4,7 @@ defmodule MathFunctionsTest do
   test "return min value from list of numbers" do
     numbers = [56, 17, 10, 10, 21, 12, 32]
     min = MathFunctions.min(numbers)
-    assert 10 == min
+    assert min == 10
   end
 
   test "return error when min is called with empty list" do
@@ -16,7 +16,7 @@ defmodule MathFunctionsTest do
   test "return max value from list of numbers" do
     numbers = [56, 17, 10, 73, 21, 12, 32]
     max = MathFunctions.max(numbers)
-    assert 73 == max
+    assert max == 73
   end
 
   test "return error when max is called with empty list" do
@@ -28,7 +28,7 @@ defmodule MathFunctionsTest do
   test "return average value from list of numbers" do
     numbers = [4, 8, 15, 16, 23, 42]
     avg = MathFunctions.avg(numbers)
-    assert 18.0 == avg
+    assert avg == 18.0
   end
 
   test "return error when avg is called with empty list" do
@@ -58,7 +58,7 @@ defmodule MathFunctionsTest do
   test "get a count of all items in a list" do
     numbers = [1, 2, 2, 2, 3, 3]
     count = MathFunctions.count(numbers)
-    assert %{1 => 1, 2 => 3, 3 => 2} == count
+    assert count == %{1 => 1, 2 => 3, 3 => 2}
   end
 
   test "return error when count is called with empty list" do
